b0515267edbd141f54cb3dada14edbcc441438a6
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2017-03-01 17:10:55 -0300

    revert mongo.sh

--- bin/mongo.sh
+++ bin/mongo.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
-docker rm -f mongo-test || true && docker run --name mongo-test -d mongo > /dev/null && sleep 2
+MONGO_ID='docker ps -aqf "name=mongo-test"'
+MONGO_IP="docker inspect --format '{{ .NetworkSettings.IPAddress }}' $(eval $MONGO_ID)"
+
+if [[ $(eval $MONGO_ID) == '' ]]; then
+  echo "Creating mongo container"
+  docker run --name mongo-test -d mongo && sleep 5
+elif [[ $(eval $MONGO_IP) == '' ]]; then
+  echo "Starting mongo container"
+  docker start mongo-test && sleep 5
+fi
