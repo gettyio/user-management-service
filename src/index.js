831ae41404506132805b04be2482235b724866c6
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2017-03-02 15:45:10 -0300

    export default route

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 import UserModel from './models/user/userModel';
 import userFactory from './models/user';
 import jwtFactory from './config/jwt';
 import passportFactory from './config/passport';
 import users from './services/user';
 
-mongoose.connect('mongodb://172.17.0.2/local');
-const app = express();
-const User = userFactory({ User: UserModel });
-const jwt = jwtFactory({ User, secret: 'somesecret' });
-const passport = passportFactory({ User, jwt });
+export default function ({ secret, unless, app }) {
+  const User = userFactory({ User: UserModel });
+  const jwt = jwtFactory({ User, secret });
+  const passport = passportFactory({ User, jwt });
 
-const UNLESS_ENDPOINTS = {
-  path: [
-    { url: /\/signup/ },
-    { url: /\/login/ }
-  ]
-};
-
-app.use(bodyParser.json());
-app.use(jwt.decodeJWT.unless(UNLESS_ENDPOINTS));
-app.use(jwt.hydrateUser.unless(UNLESS_ENDPOINTS));
-app.use(users({ passport }));
-app.use((err, req, res, next) => {
-  res.status(400).json(err);
-  next();
-});
-
-app.listen(8000, () => console.log('magic has started'));
+  app.use(jwt.decodeJWT.unless(unless));
+  app.use(jwt.hydrateUser.unless(unless));
+  return users({ passport });
+}
