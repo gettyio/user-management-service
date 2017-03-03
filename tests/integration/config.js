0de970d1e82a1b5033cd34ea7880be2abc22e278
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2017-03-03 15:05:46 -0300

    move tests to promises

--- tests/integration/config.js
+++ tests/integration/config.js
@@ -17,3 +17,5 @@ function setupMongo() {
 }
 
 setupMongo();
+
+export default mongoose;
--- tests/integration/models/user.spec.js
+++ tests/integration/models/user.spec.js
@@ -1,106 +1,137 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import '../config';
+import Promise from 'bluebird';
+import mongoose from '../config';
 import userFactory from '../../../src/models/user';
-import UserModel from '../../../src/models/user/userModel';
+import userModel from '../../../src/models/user/userModel';
 
 chai.should();
 chai.use(chaiAsPromised);
 
+const UserModel = userModel({ mongoose });
 const User = userFactory({ User: UserModel });
 
 describe('user model spec', () => {
-  beforeEach(async () => {
-    await UserModel.findOneAndRemove({ username: 'cthulhu' });
-    await UserModel.findOneAndRemove({ username: 'duplicateUser' });
+  beforeEach(() => {
+    return Promise.all([
+      UserModel.findOneAndRemove({ username: 'cthulhu' }),
+      UserModel.findOneAndRemove({ username: 'duplicateUser' })
+    ]);
   });
 
-  it('should save an user', async () => {
-    const user = await User.save({
+  it('should save an user', () => {
+    return User.save({
       username: 'cthulhu',
       password: 'abc123',
       email: 'cthulhu@example.com'
+    })
+    .then((user) => {
+      user.should.exist;
+      user.should.have.deep.property('role', 'user');
+      user.should.have.deep.property('created_at');
+      user.should.have.deep.property('updated_at');
     });
-    user.should.exist;
-    user.should.have.deep.property('role', 'user');
-    user.should.have.deep.property('created_at');
-    user.should.have.deep.property('updated_at');
   });
 
-  it('should throw an error when username is duplicate', async () => {
-    await User.save({
+  it('should throw an error when username is duplicate', () => {
+    return User.save({
       username: 'cthulhu',
       password: 'abc123',
       email: 'cthulhu@example.com'
+    })
+    .then(() =>
+      User.save({
+        username: 'cthulhu',
+        password: 'otherpass',
+        email: 'another@email.com'
+      })
+    )
+    .should.be.rejected
+    .then((error) => {
+      error.should.have.deep.property('isBoom', true);
+      error.should.have.deep.property('output.statusCode', 422);
     });
-    const result = User.save({
-      username: 'cthulhu',
-      password: 'otherpass',
-      email: 'another@email.com'
-    });
-    const error = await result.should.be.rejected;
-    error.should.have.deep.property('isBoom', true);
-    error.should.have.deep.property('output.statusCode', 422);
   });
 
-  it('should find one user by id', async () => {
-    const userSaved = await User.save({
-      username: 'cthulhu',
-      password: 'abc123',
-      email: 'cthulhu@example.com'
-    });
-    const userFound = await User.findOneById(userSaved._id);
-
-    userFound.should.be.deep.equal(userSaved);
-  });
-
-  it('should find one user by email', async () => {
-    const userSaved = await User.save({
+  it('should find one user by id', () => {
+    return User.save({
       username: 'cthulhu',
       password: 'abc123',
       email: 'cthulhu@example.com'
-    });
-    const userFound = await User.findOneByEmailOrUsername(userSaved.email);
-
-    userFound.should.be.deep.equal(userSaved);
+    })
+    .then(userSaved => [
+      userSaved,
+      User.findOneById(userSaved._id)
+    ])
+    .spread((userSaved, userFound) =>
+      userFound.should.be.deep.equal(userSaved)
+    );
   });
 
-  it('should find one user by username', async () => {
-    const userSaved = await User.save({
+  it('should find one user by email', () => {
+    return User.save({
       username: 'cthulhu',
       password: 'abc123',
       email: 'cthulhu@example.com'
-    });
-    const userFound = await User.findOneByEmailOrUsername(userSaved.username);
-
-    userFound.should.be.deep.equal(userSaved);
+    })
+    .then(userSaved => [
+      userSaved,
+      User.findOneByEmailOrUsername(userSaved.email)
+    ])
+    .spread((userSaved, userFound) =>
+      userFound.should.be.deep.equal(userSaved)
+    );
   });
 
-  it('should update an user', async () => {
-    const userSaved = await User.save({
+  it('should find one user by username', () => {
+    return User.save({
       username: 'cthulhu',
       password: 'abc123',
       email: 'cthulhu@example.com'
-    });
-
-    const userUpdated = await User.update(userSaved._id, { email: 'other_email@email.com' });
-    userUpdated.should.have.deep.property('email', 'other_email@email.com');
+    })
+    .then(userSaved => [
+      userSaved,
+      User.findOneByEmailOrUsername(userSaved.username)
+    ])
+    .spread((userSaved, userFound) =>
+      userFound.should.be.deep.equal(userSaved)
+    );
   });
 
-  it('should fail when updating with duplicate keys', async () => {
-    await User.save({
+  it('should update an user', () => {
+    return User.save({
+      username: 'cthulhu',
+      password: 'abc123',
+      email: 'cthulhu@example.com'
+    })
+    .then(userSaved =>
+      User.update(userSaved._id, { email: 'other_email@email.com' })
+    )
+    .then(userUpdated =>
+      userUpdated.should.have.deep.property('email', 'other_email@email.com')
+    );
+  });
+
+  it('should fail when updating with duplicate keys', () => {
+    return User.save({
       username: 'duplicateUser',
       password: 'abc123',
       email: 'duplicate@email.com'
+    })
+    .then(() =>
+      User.save({
+        username: 'cthulhu',
+        password: 'abc123',
+        email: 'cthulhu@example.com'
+      })
+    )
+    .then(userSaved =>
+      User.update(userSaved._id, { email: 'duplicate@email.com' })
+    )
+    .should.be.rejected
+    .then((error) => {
+      error.should.have.deep.property('isBoom', true);
+      error.should.have.deep.property('output.statusCode', 422);
     });
-    const userSaved = await User.save({
-      username: 'cthulhu',
-      password: 'abc123',
-      email: 'cthulhu@example.com'
-    });
-    const result = User.update(userSaved._id, { email: 'duplicate@email.com' });
-    const error = await result.should.be.rejected;
-    error.should.have.deep.property('isBoom', true);
-    error.should.have.deep.property('output.statusCode', 422);
   });
 });
