6d0deef07484379aea94ab69fcdeb75adf821864
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2017-03-03 17:09:52 -0300

    login tests

--- tests/integration/services/user.spec.js
+++ tests/integration/services/user.spec.js
@@ -2,6 +2,7 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import request from 'supertest';
 import express from 'express';
+import bodyParser from 'body-parser';
 import { User, UserModel } from '../config';
 import jwtFactory from '../../../src/config/jwt';
 import passportFactory from '../../../src/config/passport';
@@ -21,6 +22,7 @@ const app = express();
 const jwt = jwtFactory({ User, secret: 'somesecret' });
 const passport = passportFactory({ User, jwt });
 
+app.use(bodyParser.json());
 app.use(jwt.decodeJWT.unless(unless));
 app.use(jwt.hydrateUser.unless(unless));
 app.use(users({ passport }));
@@ -34,12 +36,58 @@ describe('user endpoints', () => {
   });
 
   it('should signup an user', () => {
-    request(app)
+    return request(app)
       .post('/signup')
-      .set('Accept', 'application/json')
+      .type('json')
+      .send({
+        username: 'cthulhu',
+        password: 'abc123',
+        email: 'cthulhu@example.com'
+      })
       .expect(201)
       .then((response) => {
-        response.should.have.deep.property('authorization');
+        response.should.have.deep.property('body.authorization');
+        response.should.have.deep.property('body.user._id');
+        response.should.have.deep.property('body.user.username');
+        response.should.have.deep.property('body.user.email');
       });
   });
+
+  it('should throw an error if username not defined', () => {
+    return request(app)
+      .post('/signup')
+      .type('json')
+      .send({
+        password: 'abc123',
+        email: 'cthulhu@example.com'
+      })
+      .expect(400);
+  });
+
+  it('should login an user', () => {
+    return request(app)
+      .post('/signup')
+      .type('json')
+      .send({
+        username: 'cthulhu',
+        password: 'abc123',
+        email: 'cthulhu@example.com'
+      })
+      .then(() => request(app)
+        .post('/login')
+        .type('json')
+        .send({
+          username: 'cthulhu',
+          password: 'abc123'
+        })
+        .expect(200)
+      )
+      .then((response) => {
+        response.should.have.deep.property('body.authorization');
+        response.should.have.deep.property('body.user._id');
+        response.should.have.deep.property('body.user.username');
+        response.should.have.deep.property('body.user.email');
+      });
+  });
+
 });
